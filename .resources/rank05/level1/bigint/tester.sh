c18652a15b548855817590576462f680911179a4
Jason TESTE <jteste@student.42mulhouse.fr>
2025-12-02 15:39:14 +0100

    okok

--- .resources/rank05/level1/bigint/tester.sh
+++ .resources/rank05/level1/bigint/tester.sh
@@ -39,7 +39,9 @@ cp "$USER_HPP" user_bigint.hpp
 cp "$USER_CPP" user_bigint.cpp
 sed 's/#include "bigint.hpp"/#include "user_bigint.hpp"/' user_main.cpp > user_main.tmp.cpp
 mv user_main.tmp.cpp user_main.cpp
-g++ -Wall -Wextra -Werror -std=c++98 -o user_bigint user_main.cpp user_bigint.cpp
+sed 's/#include "bigint.hpp"/#include "user_bigint.hpp"/' user_bigint.cpp > user_bigint.tmp.cpp
+mv user_bigint.tmp.cpp user_bigint.cpp
+g++ -Wall -Wextra -Werror -std=c++98 -o user_bigint user_main.cpp user_bigint.cpp 
 if [ $? -ne 0 ]; then
     echo -e "${RED}❌ User compilation failed!${NC}"
     exit 1
